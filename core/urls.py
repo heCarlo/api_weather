425391e28cc193ec0a62d2dfa01eba3042b28ac3
heCarlo <carlosgfkp@outlook.com>
2024-04-10 00:31:01 -0300

    feat(urls): updated urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from weather.views import WeatherView, WeatherGenerate
+from weather.views.weatherView import WeatherCreate, WeatherReset, WeatherView, WeatherGenerate, WeatherUpdate, WeatherDelete
+from uuid import UUID
 
 urlpatterns = [
-    path('', WeatherView.as_view(), name='Weather View'),
-    path('generate', WeatherGenerate.as_view(), name='Weather Generate'),
+    path('weather/', WeatherView.as_view(), name='weather-view'),
+    path('weather/generate/', WeatherGenerate.as_view(), name='weather-generate'),
+    path('weather/update/<str:uuid>/', WeatherUpdate.as_view(), name='weather-update'),
+    path('weather/delete/<str:uuid>/', WeatherDelete.as_view(), name='weather-delete'),
+    path('weather/reset/', WeatherReset.as_view(), name='weather-reset'),  
+    path('weather/create/', WeatherCreate.as_view(), name='weather-create'), 
 ]
