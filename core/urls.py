b27c2fd5d59bd5ec4231913865dcdd3cc3b7d666
heCarlo <carlosgfkp@outlook.com>
2024-05-23 21:58:00 -0300

    feat: added urls for user login and register

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from user.userView import UserLogin, UserRegister
 from weather.views.weatherView import WeatherCreate, WeatherReset, WeatherView, WeatherGenerate, WeatherUpdate, WeatherDelete
-from uuid import UUID
+from rest_framework_simplejwt.views import TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('weather/', WeatherView.as_view(), name='weather-view'),
@@ -8,5 +13,11 @@ urlpatterns = [
     path('weather/update/<str:uuid>/', WeatherUpdate.as_view(), name='weather-update'),
     path('weather/delete/<str:uuid>/', WeatherDelete.as_view(), name='weather-delete'),
     path('weather/reset/', WeatherReset.as_view(), name='weather-reset'),  
-    path('weather/create/', WeatherCreate.as_view(), name='weather-create'), 
+    path('weather/create/', WeatherCreate.as_view(), name='weather-create'),
+    path('login/', UserLogin.as_view(), name='login'),
+    path('register/', UserRegister.as_view(), name='register'),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
